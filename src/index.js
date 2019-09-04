a71526c7c3534073a89010a7a2105a5cfb16f3e7
Unknown <mangastens@gmail.com>
2019-09-04 04:09:52 +0200

    FirstCommit

---
+++ src/index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const Strategy = require('passport-http').DigestStrategy;
+passport = require("passport")
+//Så res.body funkar 
+app.use(express.urlencoded());
+app.use(express.static("public"));
+app.use(express.json());
+app.use(passport.initialize());
+
+
+// Access the parse results as request.body
+app.post('/', function(request, response){
+    
+    console.log(request.body.name);
+    response.send("dicktum")
+    
+});
+
+passport.use(new Strategy({ qop: 'auth' },
+  function(username, done) {
+    return done(null,"123","124")
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      return done(null, user, user.password);
+    });
+  },
+  function(params, done) {
+    // validate nonces as necessary
+    done(null, true)
+  }
+));
+app.get('/',
+  passport.authenticate('digest', { session: false }),
+  function(req, res) {
+    res.json(req.user);
+  });
+
+
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
